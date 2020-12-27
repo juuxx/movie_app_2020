34b4fffd1d46c14066763dfb936251ae8347bc1c
myeongjooseo <juux_x@naver.com>
2020-12-27 12:59:28 +0900

    potato.js 만들기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import Potato from "./potato";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//ReactDOM.render(<App /><Potato/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//react application 이 하나의 component만 rendering 해야함.
+//얘의 역할은 app만 렌더링
+//-> app안에 potato를 넣어보자
+
